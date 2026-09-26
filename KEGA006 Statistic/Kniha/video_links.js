6d383f8880ee66dc3fa974a8a8b255cbc13f074b
fairchild-project <horehore333@gmail.com>
2026-09-26 15:57:44 +0200

    Update video_links.js

--- KEGA006 Statistic/Kniha/video_links.js
+++ KEGA006 Statistic/Kniha/video_links.js
@@ -12,69 +12,69 @@ window.STATISTICS_VIDEO_LINKS = {
     "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-01/kap1_segment6.mp4"
   },
   "kap2": {
-    "segment1": "VLOZ_LINK_KAP2_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP2_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP2_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP2_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP2_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP2_SEGMENT6"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-02/kap2_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-02/kap2_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-02/kap2_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-02/kap2_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-02/kap2_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-02/kap2_segment6.mp4"
   },
   "kap3": {
-    "segment1": "VLOZ_LINK_KAP3_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP3_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP3_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP3_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP3_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP3_SEGMENT6",
-    "segment7": "VLOZ_LINK_KAP3_SEGMENT7",
-    "segment8": "VLOZ_LINK_KAP3_SEGMENT8"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment6.mp4",
+    "segment7": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment7.mp4",
+    "segment8": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-03/kap3_segment8.mp4"
   },
   "kap4": {
-    "segment1": "VLOZ_LINK_KAP4_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP4_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP4_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP4_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP4_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP4_SEGMENT6"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-04/kap4_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-04/kap4_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-04/kap4_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-04/kap4_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-04/kap4_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-04/kap4_segment6.mp4"
   },
   "kap5": {
-    "segment1": "VLOZ_LINK_KAP5_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP5_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP5_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP5_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP5_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP5_SEGMENT6"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-05/kap5_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-05/kap5_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-05/kap5_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-05/kap5_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-05/kap5_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-05/kap5_segment6.mp4"
   },
   "kap6": {
-    "segment1": "VLOZ_LINK_KAP6_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP6_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP6_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP6_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP6_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP6_SEGMENT6"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-06/kap6_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-06/kap6_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-06/kap6_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-06/kap6_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-06/kap6_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-06/kap6_segment6.mp4"
   },
   "kap7": {
-    "segment1": "VLOZ_LINK_KAP7_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP7_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP7_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP7_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP7_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP7_SEGMENT6"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-07/kap7_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-07/kap7_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-07/kap7_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-07/kap7_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-07/kap7_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-07/kap7_segment6.mp4"
   },
   "kap8": {
-    "segment1": "VLOZ_LINK_KAP8_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP8_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP8_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP8_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP8_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP8_SEGMENT6"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-08/kap8_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-08/kap8_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-08/kap8_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-08/kap8_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-08/kap8_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-08/kap8_segment6.mp4"
   },
   "kap9": {
-    "segment1": "VLOZ_LINK_KAP9_SEGMENT1",
-    "segment2": "VLOZ_LINK_KAP9_SEGMENT2",
-    "segment3": "VLOZ_LINK_KAP9_SEGMENT3",
-    "segment4": "VLOZ_LINK_KAP9_SEGMENT4",
-    "segment5": "VLOZ_LINK_KAP9_SEGMENT5",
-    "segment6": "VLOZ_LINK_KAP9_SEGMENT6"
+    "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment1.mp4",
+    "segment2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment2.mp4",
+    "segment3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment3.mp4",
+    "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment4.mp4",
+    "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment5.mp4",
+    "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment6.mp4"
   }
 };
