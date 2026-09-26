b22030e965f987930b43af23d86824ca744bc14b
fairchild-project <horehore333@gmail.com>
2026-09-26 16:09:25 +0200

    Update video_links.js

--- KEGA006 Statistic/Kniha/video_links.js
+++ KEGA006 Statistic/Kniha/video_links.js
@@ -1,7 +1,8 @@
 /*
-  JEDINY SUBOR, KDE RUCNE VKLADATE LINKY NA VIDEO SEGMENTY.
-  Po uprave spustite sync_video_links.bat, aby sa kopia vlozila do github/.
+  JEDINY SUBOR PRE VSETKY VIDEO LINKY.
+  Ak sa videa neskor presunu na iny server, upravte iba URL v tomto subore.
 */
+
 window.STATISTICS_VIDEO_LINKS = {
   "kap1": {
     "segment1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-01/kap1_segment1.mp4",
@@ -76,5 +77,92 @@ window.STATISTICS_VIDEO_LINKS = {
     "segment4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment4.mp4",
     "segment5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment5.mp4",
     "segment6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/SSD%20segmenty/kapitola-09/kap9_segment6.mp4"
+  }
+};
+
+window.STATISTICS_HELPER_VIDEO_LINKS = {
+  "kap1": {
+    "1-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola1/1.1%20Co%20je%20vlastne%20dataset.mp4",
+    "1-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola1/1.2%20Kvalitativna%20vs%20kvantitativna.mp4",
+    "1-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola1/1.3%20Nominalna%20vs%20ordinalna.mp4",
+    "1-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola1/1.4%20Diskretna%20vs%20spojita.mp4",
+    "1-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola1/1.5%20Cislo%20este%20neznamena%20meranie.mp4",
+    "1-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola1/1.6%20Aky%20typ%20premennej%20aka%20metoda.mp4",
+    "1-7": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola1/1.7%20Premenne%20mozu%20byt%20prepojene.mp4"
+  },
+  "kap2": {
+    "2-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola2/2.1%20Absolutna%20vs%20relativna%20pocetnost.mp4",
+    "2-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola2/2.2%20Modus%20najcastejsia%20kategoria.mp4",
+    "2-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola2/2.3%20Kumulativna%20pocetnost.mp4",
+    "2-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola2/2.4%20Kontingencna%20tabulka.mp4",
+    "2-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola2/2.5%20Riadkove%20vs%20stlpcove%20percenta.mp4",
+    "2-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola2/2.6%20Graf%20pocetnosti.mp4",
+    "2-7": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola2/2.7%20Pocetnost%20nie%20je%20dokaz%20vztahu.mp4"
+  },
+  "kap3": {
+    "3-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.1%20Empiricka%20pravdepodobnost.mp4",
+    "3-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.2%20Doplnok%20udalosti.mp4",
+    "3-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.3%20Prienik%20a%20zjednotenie.mp4",
+    "3-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.4%20Podmienena%20pravdepodobnost.mp4",
+    "3-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.5%20P%20H%20A%20nie%20je%20P%20A%20H.mp4",
+    "3-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.6%20Zakon%20uplnej%20pravdepodobnosti.mp4",
+    "3-7": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.7%20Bayesova%20veta%20bez%20strachu.mp4",
+    "3-8": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.8%20Viac%20pokusov%20stabilnejsia%20pravdepodobnost.mp4",
+    "3-9": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola3/3.9%20Normalne%20rozdelenie%20intuitivne.mp4"
+  },
+  "kap4": {
+    "4-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.1%20Vyber%20vs%20populacia.mp4",
+    "4-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.2%20Bodovy%20odhad.mp4",
+    "4-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.3%20SD%20vs%20SE.mp4",
+    "4-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.4%20Co%20znamena%2095%20interval%20spolahlivosti.mp4",
+    "4-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.5%20Konkretny%20interval%20systolicky%20tlak.mp4",
+    "4-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.6%20Preco%20sa%20interval%20zuzuje.mp4",
+    "4-7": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.7%20Nie%20kazdy%20interval%20rovnako%20jednoduchy.mp4",
+    "4-8": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola4/4.8%20Typy%20rozdeleni%20dat.mp4"
+  },
+  "kap5": {
+    "5-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.1%20H0%20vs%20H1%20logika%20testu.mp4",
+    "5-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.2%20P%20hodnota%20vizualne.mp4",
+    "5-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.3%20Chyba%20I%20a%20II%20druhu.mp4",
+    "5-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.4%20Jednovyberovy%20t%20test%20tlak%20120.mp4",
+    "5-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.5%20Dvojvyberovy%20t%20test%20BMI.mp4",
+    "5-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.6%20Parovy%20t%20test%20pred%20po.mp4",
+    "5-7": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.7%20Shapiro%20Wilk%20normalita.mp4",
+    "5-8": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.8%20Fisher%20vs%20Chi%20square.mp4",
+    "5-9": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola5/5.9%20Parametricke%20vs%20neparametricke%20testy.mp4"
+  },
+  "kap6": {
+    "6-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola6/6.1%20Preco%20ANOVA%20a%20nie%20vela%20t%20testov.mp4",
+    "6-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola6/6.2%20Odkial%20sa%20berie%20F%20statistika.mp4",
+    "6-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola6/6.3%20Jednofaktorova%20ANOVA%20tlak%20podla%20fajcenia.mp4",
+    "6-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola6/6.4%20Tukey%20post%20hoc.mp4",
+    "6-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola6/6.5%20Dvojfaktorova%20ANOVA%20BMI.mp4",
+    "6-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola6/6.6%20Interakcia.mp4"
+  },
+  "kap7": {
+    "7-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.1%20Regresia%20vs%20korelacia.mp4",
+    "7-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.2%20Jednoducha%20linearna%20regresia.mp4",
+    "7-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.3%20Intercept%20koeficient%20a%20reziduum.mp4",
+    "7-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.4%20R2%20kolko%20variability%20vysvetluje%20model.mp4",
+    "7-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.5%20Viacnasobna%20regresia.mp4",
+    "7-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.6%20Dummy%20premenne%20a%20referencna%20kategoria.mp4",
+    "7-7": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.7%20Diagnostika%20regresie.mp4",
+    "7-8": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola7/7.8%20Typy%20regresie.mp4"
+  },
+  "kap8": {
+    "8-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola8/8.1%20Co%20hovori%20korelacny%20koeficient.mp4",
+    "8-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola8/8.2%20Pearson%20vek%20a%20systolicky%20tlak.mp4",
+    "8-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola8/8.3%20Pearson%20vs%20Spearman.mp4",
+    "8-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola8/8.4%20Korelacna%20matica.mp4",
+    "8-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola8/8.5%20Sila%20vztahu%20vs%20p%20hodnota.mp4",
+    "8-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola8/8.6%20Korelacia%20nie%20je%20kauzalita.mp4"
+  },
+  "kap9": {
+    "9-1": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola9/9.1%20Co%20je%20casovy%20rad.mp4",
+    "9-2": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola9/9.2%20Trend%20sezonnost%20a%20nahodna%20zlozka.mp4",
+    "9-3": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola9/9.3%20Jednoduchy%20linearny%20trend%20CRP.mp4",
+    "9-4": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola9/9.4%20Sezonnost%20a%20dummy%20premenne.mp4",
+    "9-5": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola9/9.5%20Predikcia%2013%20mesiaca.mp4",
+    "9-6": "https://maksym.mykhei.website.tuke.sk/KEGA006Statistick/Kapitola9/9.6%20Preco%20jeden%20rok%20nestaci.mp4"
   }
 };
